be924c9d657b512c6d3b2139721fbd1593582066
Paul Brubaker <pbrubak@gmail.com>
2021-08-03 16:50:00 -0700

    add option ro revert filters

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ import CreatureOptions from './Components/CreatureOptions';
 import './App.css';
 
 class App extends Component {
-    state = { keyword: 'chameleon' }
+    state = { 
+                keyword: false,
+                numHorns: false
+            }
 
-    upDateSelect = (e) => {
+    updateKeyword = (e) => {
         this.setState({
-            keyword: e.target.value
+            keyword: e.target.value,
+            numHorns: this.state.numHorns
+        })
+    }
+
+    updateHorns = (e) => {
+        this.setState({
+            keyword: this.state.keyword,
+            numHorns: +e.target.value
+        })
+    }
+
+    handleRemoveFilters = () => {
+        this.setState({
+            keyword: false,
+            numHorns: false
         })
     }
 
@@ -17,15 +35,24 @@ class App extends Component {
             const options = [...new Set(creatures.map(creature => creature.keyword))];
             return ( 
                 <>
-                <select name="creature-select" onChange={this.upDateSelect}>
+                <select name="creature-select" onChange={this.updateKeyword}>
                     <CreatureOptions 
                         options={options}
                     />
                 </select>
+                <select name="horn-select" onChange={this.updateHorns}>
+                    <CreatureOptions
+                        options={[1, 2, 3, 100]}
+                    />
+                </select>
+                <button onClick={this.handleRemoveFilters}>Remove Filters</button>
                 <ImageList 
                     creatureImages={creatures.filter(
                         (creature) => {
-                            return creature.keyword === this.state.keyword;
+                            return this.state.keyword ? creature.keyword === this.state.keyword : true;
+                        }).filter(
+                        (creature) => {
+                            return this.state.numHorns ? creature.horns === this.state.numHorns : true;
                         }
                     )}
                 />
